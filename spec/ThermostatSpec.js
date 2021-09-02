fab2840635d7baa335cd652ae17ec1c078bde063
edpackard <edpackard81@gmail.com>
2021-09-02 17:09:35 +0100

    implementing power saving functionality

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -20,10 +20,41 @@ describe ("Thermostat", () => {
   });
 
   it("expects down to decrease the temperature by 1 degree", () => {
-    for(let i = 1; i < 15; i++) {
+    for(let i = 0; i < 15; i++) {
       thermostat.down();
     }
     expect(thermostat.temp).toEqual(10);
   });
+
+  it("expect a new thermostat to start in power saving mode", () => {
+    expect(thermostat.isPowerSaving()).toBe(true);
+  });
+
+  it("can turn off powersaving mode", () => {
+    thermostat.powerSaving()
+    expect(thermostat.isPowerSaving()).toBe(false);
+  });
+
+  it("can turn on powersaving mode", () => {
+    for (let i = 0; i < 2; i++) {
+      thermostat.powerSaving()
+    }
+    expect(thermostat.isPowerSaving()).toBe(true); 
+  });
+
+  it("can't go above 25 degrees if powersaving on", () => {
+    for(let i = 0; i < 10; i++) {
+      thermostat.up();
+    }
+    expect(thermostat.temp).toEqual(25);
+  }); 
+
+  it("can't go above 32 degrees if powersaving off", () => {
+    thermostat.powerSaving()
+    for(let i = 0; i < 20; i++) {
+      thermostat.up();
+    }
+    expect(thermostat.temp).toEqual(32);
+  }); 
 
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -3,7 +3,14 @@ class Thermostat {
   constructor() {
     this.DEFAULT_TEMP = 20;
     this.MINIMUM_TEMP = 10;
+    this.POWERSAVE_MAX_TEMP = 25;
+    this.NORMAL_MAX_TEMP = 32;
     this.temp = this.DEFAULT_TEMP;
+    this.powerSavingMode = true;
+  }
+
+  isPowerSaving() {
+    return this.powerSavingMode
   }
 
   temp() {
@@ -11,7 +18,17 @@ class Thermostat {
   }
 
   up() {
-    this.temp++
+    let powersave = (this.isPowerSaving() && this.temp < this.POWERSAVE_MAX_TEMP)
+    let normal = (!this.isPowerSaving() && this.temp < this.NORMAL_MAX_TEMP)
+    if ( powersave || normal ) {
+      this.temp++
+    }
+    // if (this.isPowerSaving() && this.temp < this.POWERSAVE_MAX_TEMP) {
+    //   this.temp++
+    // }
+    // if (!this.isPowerSaving() && this.temp < this.NORMAL_MAX_TEMP) {
+    //   this.temp++
+    // }
   }
 
   down() {
@@ -19,4 +36,10 @@ class Thermostat {
     this.temp--
     }
   }
+
+  powerSaving() {
+    this.powerSavingMode = this.powerSavingMode ? false : true
+  }
+
+
 }
