7b0d59f6145ea93e370e9c4d7e640a8c61977bb3
William Day <williamjday11@gmail.com>
2021-09-02 16:06:12 +0100

    Added up and down methods and with minimum temperature

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -6,7 +6,24 @@ describe ("Thermostat", () => {
   });
 
   it("expects a new thermostat to have a starting temp of 20", () => {
-    expect(thermostat.temp()).toEqual(20);
+    expect(thermostat.temp).toEqual(20);
+  });
+
+  it("expects up to increase the temperature by 1 degree", () => {
+    thermostat.up();
+    expect(thermostat.temp).toEqual(21);
+  });
+
+  it("expects down to decrease the temperature by 1 degree", () => {
+    thermostat.down();
+    expect(thermostat.temp).toEqual(19);
+  });
+
+  it("expects down to decrease the temperature by 1 degree", () => {
+    for(let i = 1; i < 15; i++) {
+      thermostat.down();
+    }
+    expect(thermostat.temp).toEqual(10);
   });
 
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,5 +1,22 @@
 class Thermostat {
+
+  constructor() {
+    this.DEFAULT_TEMP = 20;
+    this.MINIMUM_TEMP = 10;
+    this.temp = this.DEFAULT_TEMP;
+  }
+
   temp() {
-    return (20);
+    return this.temp
+  }
+
+  up() {
+    this.temp++
+  }
+
+  down() {
+    if (this.temp > this.MINIMUM_TEMP) {
+    this.temp--
+    }
   }
 }
